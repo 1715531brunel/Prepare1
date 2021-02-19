2061331cd2f5574dc0d2d8b46205ead68beaba44
1715531brunel <1715531@brunel.ac.uk>
2021-02-19 11:48:56 +0000

    new project

---
+++ app/src/main/java/com/example/prepare/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.prepare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         //Code navigates through the pages from navigation bar
+
+    }
+
+
+}

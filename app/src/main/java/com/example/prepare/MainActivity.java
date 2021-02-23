c774d69dc65d863bee7a6a1dcb3261f93235deb8
1715531brunel <1715531@brunel.ac.uk>
2021-02-23 13:40:15 +0000

    Semi working card view is clickable

--- app/src/main/java/com/example/prepare/MainActivity.java
+++ app/src/main/java/com/example/prepare/MainActivity.java
@@ -2,30 +2,49 @@ package com.example.prepare;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
          //Code navigates through the pages from navigation bar
         BottomNavigationView bottomNav = findViewById(R.id.bottomBar);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_out, new HomeFragment()).commit();
 
+
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    //Code for cardview on homepage to navigate next page
+    public void OpenQuestionPage(View view){
+        Intent i = new Intent(this, question_page.class);
+        startActivity(i);
+    }
+
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment selectedFragment = null;
-
+ // switch statements for selected icons on bottom navigation
             switch (item.getItemId()){
                 case R.id.nav_home:
                     selectedFragment = new HomeFragment();
---
+++ app/src/main/java/com/example/prepare/support_page.java
@@ -0,0 +1,14 @@
+package com.example.prepare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class support_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_page);
+    }
+}

0ed3fe1d466ccd051e9de15ba1ee1b2431c8d61f
1715531brunel <1715531@brunel.ac.uk>
2021-03-23 17:24:49 +0000

    working calcute score

--- app/src/main/java/com/example/prepare/AssessActivity.java
+++ app/src/main/java/com/example/prepare/AssessActivity.java
@@ -2,6 +2,7 @@ package com.example.prepare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,7 @@ public class AssessActivity extends AppCompatActivity {
     private RadioButton radbt4;
     private RadioButton radbt5;
     private Button btNext;
+
     private List<Quest> questList;
     private int questCounter;
     private int questTotalCount;
@@ -52,27 +54,82 @@ public class AssessActivity extends AppCompatActivity {
         questList = dbHelper.getAllQuests();
         questTotalCount = questList.size();
 
+
         showNxtQuest();
+        btNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!answrd) {
+                    if (radbt1.isChecked()) {
+                        scores += 5;
+                        checkAns();
+
+                    } else if (radbt2.isChecked()) {
+                        scores += 4;
+                        checkAns();
+
+                    } else if (radbt3.isChecked()) {
+                        scores += 3;
+
+                        checkAns();
+
+                    }else if(radbt4.isChecked()){
+                        scores += 2;
+
+                        checkAns();
+
+                    }else if(radbt5.isChecked()){
+                        scores += 1;
+                        checkAns();
+
+                    }else{
+                        Toast.makeText(AssessActivity.this,"Please choose an answer",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    showNxtQuest();
+                }
+
+            }
+        });
 
     }
 
     private void showNxtQuest() {
+
         radGrop.clearCheck();
+
         if (questCounter < questTotalCount) {
             currentQuest = questList.get(questCounter);
+
             textViewQuest.setText(currentQuest.getQuest());
             radbt1.setText(currentQuest.getOpt1());
             radbt2.setText(currentQuest.getOpt2());
             radbt3.setText(currentQuest.getOpt3());
             radbt4.setText(currentQuest.getOpt4());
             radbt5.setText(currentQuest.getOpt5());
+
             questCounter++;
             textViewQuestCount.setText("Question: " + questCounter + "/" + questTotalCount);
             answrd = false;
+            btNext.setText("Confirm");
 
-        }else{
+        } else {
             finishAssess();
         }
+    }
+    private void checkAns(){
+        answrd = true;
+        textViewScore.setText("Score: " + scores);
+        showSol();
+    }
+
+    private void showSol() {
+        if (questCounter < questTotalCount) {
+            btNext.setText("Next");
+        } else {
+            btNext.setText("Finish");
+        }
+
     }
 
     private void finishAssess() {
